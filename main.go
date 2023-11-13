f806a08ed82099b9d2bd88e99fd39eac070975a8
ndeta100 <ndetainnocent3@gmail.com>
2023-11-13 21:47:59 +0200

    Creatong a server to serve http and a storage

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+)
+
+//Underlying storage (in memory, on disk, s3, anything
+//server (http, tcp)
 func main() {
+	cfg := &Config{
+		ListenAddr: ":3000",
+		StoreProducerFunc: func() Storer {
+			return NewMemoryStore()
+		},
+	}
+	s, err := NewServer(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Start()
 
 }
